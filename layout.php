8c5fc703884c002dd3d0ab8d380a261aae0d1794
Yvar-School <yvar.nanlohij001@fclive.nl>
2020-05-18 09:32:31 +0200

    l

--- layout.php
+++ layout.php
@@ -3,9 +3,9 @@
 <head>
   <link rel="stylesheet" href="./css/style.css">
   <meta charset="UTF-8">
-	
-	<title>Weer-API Jarno Yvar</title>
-	<link rel="shortcut icon" href="./favicon.ico"/>
+
+  <title>Weer-API Jarno Yvar</title>
+  <link rel="shortcut icon" href="./favicon.ico" />
 
   <script src="https://kit.fontawesome.com/35a0bcef4b.js" crossorigin="anonymous"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,7 +13,7 @@
   <script src="https://openlayers.org/en/v4.6.5/build/ol.js"></script>
   <script src="https://nlmaps.nl/dist/nlmaps.iife.js"></script>
   <script src="./js/script.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
 </head>
 
 <div id="viewport">
@@ -35,10 +35,16 @@
           Kaart
         </a>
       </li>
-	  <li>
+      <li>
         <a href="graphs.php">
           <i class="fas fa-chart-line"></i>
           Grafieken
+        </a>
+      </li>
+      <li>
+        <a href="buien.php">
+          <i class="fas fa-cloud-rain"></i>
+          Buien Radar
         </a>
       </li>
   </div>
